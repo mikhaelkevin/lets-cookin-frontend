a7b8972e6dfdbf778e429f67eabbd97a1253c4c8
Mikhael <goodboybutnot@gmail.com>
2022-09-13 05:57:50 +0700

    feature(redux): redux implementation on auth user.
    adding new redux for login and implement persist on it.
    
    feature rtk-persist.

---
+++ src/redux/store.js
@@ -0,0 +1,34 @@
+import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import reducers from './reducers';
+
+const persistConfig = {
+  key: 'letscookin',
+  storage,
+  version: 1,
+  whitelist: ['auth'],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const reduxStore = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+  devTools: process.env.NODE_ENV !== 'production',
+});
+
+export const persistor = persistStore(reduxStore);
